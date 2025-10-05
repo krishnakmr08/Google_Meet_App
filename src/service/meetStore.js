0e941cbb0778850f2337c6f5f97485cd5e90857e
Krishna Kumar <kkbhagat20@gmail.com>
2025-10-05 23:32:23 +0530

    create zustand meetStore

---
+++ src/service/meetStore.js
@@ -0,0 +1,84 @@
+import { create } from 'zustand';
+import { createJSONStorage } from 'zustand/middleware';
+import { mmkvStorage } from './storage';
+
+export const useLiveMeetStore = create()(
+  (set, get) => ({
+    sessionId: null,
+    participants: [],
+    micOn: false,
+    videoOn: false,
+    clear: () =>
+      set({
+        sessionId: null,
+        participants: [],
+      }),
+
+    addSessionId: id => {
+      set({ sessionId: id });
+    },
+    removeSessionId: () => {
+      set({ sessionId: null });
+    },
+
+    addParticipant: participant => {
+      const { participants } = get();
+
+      if (!participants.find(p => p.userId === participant?.userId)) {
+        set({ participants: [...participants, participant] });
+      }
+    },
+
+    removePartcipant: participantId => {
+      const { participants } = get();
+
+      set({
+        participants: participants.filter(p => p.userId !== participantId),
+      });
+    },
+
+    updateParticipant: updatedParticipant => {
+      const { participants } = get();
+
+      set({
+        participants: participants.map(p =>
+          p.userId === updatedParticipant.userId
+            ? {
+                ...p,
+                micOn: updatedParticipant.micOn,
+                videoOn: updatedParticipant.videoOn,
+              }
+            : p,
+        ),
+      });
+    },
+
+    setStreamURL: (participantId, streamURL) => {
+      const { participants } = get();
+      const updatedParticipants = participants.map(p => {
+        if (p.userId === participantId) {
+          return { ...p, streamURL };
+        }
+        return p;
+      });
+
+      // if (!participants.some(p => p.userId === participantId)) {
+      //   updatedParticipants.push({ id: participantId, streamURL });
+      // }
+
+      set({ participants: updatedParticipants });
+    },
+
+    toggle: type => {
+      if (type === 'mic') {
+        set(state => ({ micOn: !state.micOn }));
+      } else if (type === 'video') {
+        set(state => ({ videoOn: !state.videoOn }));
+      }
+    },
+  }),
+  {
+    name: 'live-meet-storage',
+    storage: createJSONStorage(() => mmkvStorage),
+  },
+);
